dbce90185258788521796ada127db65db31ac5a8
Yousuf-22 <yousuf.sayyed.cse@ghrcemp.raisoni.net>
2024-08-08 17:21:41 +0530

    newQ

--- Second Month/intersection.cpp
+++ Second Month/intersection.cpp
@@ -1,31 +1,36 @@
 #include<iostream>
 #include<vector>
-#include<map>
-#include<set>
+#include<algorithm>
+#include<unordered_map>
 using namespace std;
 vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    map<int,int> mpp1;
-    map<int,int> mpp2;
-
-    for(int i: nums1) {
-        mpp1[i]++;
-    }
-
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
+    unordered_map<int,int> mpp;
     vector<int> ans;
-    set<int> uniqueset;
-    for(int i : nums2) {
-        if(i == mpp1[i]) {
-            
-            ans.push_back(i);
+    int i = 0;
+    int j = 0;
+
+    while(i != nums1.size() and j != nums2.size()) {
+        if(nums1[i] == nums2[j]) {
+            mpp[nums1[i]]++;
+            i++;
+            j++;
         }
-    }
-    for(int x : ans) {
-        cout << x << " ";
+        if(nums1[i] > nums2[j]) {
+            j++;
+        }
+        if(nums1[i] < nums2[j]) {
+            i++;
+        }
+    } 
+    for(auto it : mpp) {
+        ans.push_back(it.first);
     }
 }
 int main() {
-    vector<int> v1 = {1,2,2,1};
-    vector<int> v2 = {2,2};
-    intersection(v1,v2);
+    vector<int> arr = {1,2,2,1};
+    vector<int> brr = {2,2};
+    intersection(arr,brr);
     return 0;
 }
