64f778caf0f25287a6bd2c582293770dbf915c40
Yousuf-22 <yousuf.sayyed.cse@ghrcemp.raisoni.net>
2024-09-13 19:21:48 +0530

    newQ

--- Second Month/kthSmallestPrimeFraction.cpp
+++ Second Month/kthSmallestPrimeFraction.cpp
@@ -3,26 +3,20 @@
 #include<queue>
 using namespace std;
 vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-    priority_queue<pair<int, pair<int,int>>> maxx;
-    vector<pair<int,int>> prr;
+    priority_queue<pair<double, pair<int,int>>> maxx;
+
 
     vector<int> ans;
     for(int i = 0; i<arr.size(); i++) {
         for(int j = i+1; j<arr.size(); j++) {
-            prr.push_back({arr[i],arr[j]});
-        }
-    }        
+            double val = ((double)arr[i]/(double)arr[j]);
+            maxx.push({val,{arr[i],arr[j]}});
 
-    for(int i = 0; i<prr.size(); i++) {
-        double x = prr[i].first;
-        double y = prr[i].second;
-        int ans  = (x/y)*10;
-        maxx.push({ans,{prr[i].first,prr[i].second}});
-
-        if(maxx.size() > k) {
-            maxx.pop();
+            if(maxx.size() > k) {
+                maxx.pop();
+            }
         }
-    }
+    }         
 
     int x = maxx.top().second.first;
     int y = maxx.top().second.second;
@@ -33,8 +27,8 @@ vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
     return ans;
 }
 int main() {
-    vector<int> arr = {1,13,17,59};
-    vector<int> ans = kthSmallestPrimeFraction(arr,6);
+    vector<int> arr = {1,29,47};
+    vector<int> ans = kthSmallestPrimeFraction(arr,1);
 
     for(int i : ans) {
         cout << i << " ";
