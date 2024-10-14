1a887aef639dbee07126369091e443b05e2ebb9a
Yousuf-22 <yousuf.sayyed.cse@ghrcemp.raisoni.net>
2024-10-14 11:14:26 +0530

    Daily Ques

--- Third Month/addBinary.cpp
+++ Third Month/addBinary.cpp
@@ -2,20 +2,22 @@
 #include<algorithm>
 using namespace std;
 int deci(string a) {
-    int p = 1;
-    int ans = 0;
+    long long p = 1;
+    long long ans = 0;
     for(int i = a.size()-1; i>=0; i--) {
         if(a[i] == '1') {
             ans = ans + p;
         }
+        if(p < INT16_MAX)
         p *= 2;
     }
     return ans;
 }
 string addBinary(string a, string b) {
-    int x = deci(a);
-    int y = deci(b);
-    int z = x + y;
+    if(a[0] == '0' and b[0] == '0') return "0";
+    long long x = deci(a);
+    long long y = deci(b);
+    long long z = x + y;
 
     string str = "";
     while(z > 0) {
@@ -31,6 +33,6 @@ string addBinary(string a, string b) {
     return str;
 }
 int main() {
-    cout << addBinary("1010","1011");
+    cout << addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101","110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011");
     return 0;
 }
